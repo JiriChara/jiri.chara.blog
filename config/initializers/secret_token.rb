f95b2e91cb9dbbc85b3b9c7ebca055c25657a70c
Jiri Chara <jirik.chara@gmail.com>
2013-11-01 22:31:43 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JiriCharaBlog::Application.config.secret_key_base = 'c135d702568426c0b804f34b369ea3de442f0b2ace64ffca64bd778e11572c4f7ffdbd8c6808cf35537806544c57e8e1d2e57043c30e3bb468213aba1bfc3f35'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JiriCharaBlog::Application.config.session_store :cookie_store, key: '_jiri_chara_blog_session'
