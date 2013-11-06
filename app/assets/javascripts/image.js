d9bd3e5e69bf8c0cea929b97ddd2efcda03e3320
Jiri Chara <jirik.chara@gmail.com>
2013-11-06 14:02:36 +0100

    Images upload

---
+++ app/assets/javascripts/image.js
@@ -0,0 +1,28 @@
+$(function () {
+    $('#new_image').fileupload({
+        dataType: "json",
+        add: function(e, data) {
+            if($('#article-images').is(':hidden')) {
+                $('#article-images').show();
+            }
+
+            var file = data.files[0];
+
+            data.context = $(tmpl("template-upload", file));
+
+            $('#article-images').append(data.context);
+
+            data.submit();
+        },
+        progress: function(e, data) {
+            if(data.context) {
+                var progress = parseInt(data.loaded / data.total * 100, 10);
+                data.context.find('.bar').css('width', progress + '%')
+
+            }
+        },
+        done: function(e, data) {
+            data.context.html('');
+        }
+    });
+});
---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,42 @@
+class ImagesController < ApplicationController
+  authorize_resource
+
+  def index
+    @images = Image.all
+
+    render json: @images.collect { |i| i.to_jq_upload }.to_json
+  end
+
+  def create
+    if params[:image][:image].is_a?(Array)
+      params[:image][:image] = params[:image][:image][0]
+    end
+
+    @image = Image.new(params.require(:image).permit(
+      :image, :article_id
+    ))
+
+    if @image.save
+      respond_to do |format|
+        format.html do
+          render json: [@image.to_jq_upload].to_json,
+            content_type: "text/html",
+            layout: false
+        end
+
+        format.json do
+          render json: [@image.to_jq_upload].to_json
+        end
+      end
+    else
+      render json: [{ error: "Upload failure" }], status: 304
+    end
+  end
+
+  def destroy
+    @imsage = Image.find(params[:id])
+
+    @image.destroy
+    render json: true
+  end
+end
