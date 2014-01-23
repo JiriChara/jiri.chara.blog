98071a3262efba584b04bd9218403c7e158e8574
Jiri Chara <jirik.chara@gmail.com>
2014-01-23 11:57:08 +0100

    Added edit action to tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,13 +5,33 @@ class TagsController < ApplicationController
     @tags = Tag.all
   end
 
+  def edit
+    @tag = Tag.find_by(slug: params[:id])
+  end
+
+  def update
+    @tag = Tag.find_by(slug: params[:id])
+
+    if @tag.update(tag_params)
+      flash[:success] = "Tag was successfully updated."
+      redirect_to tags_path
+    else
+      render :edit
+    end
+  end
+
   def destroy
-    @tag = Tag.find(params[:id])
+    @tag = Tag.find_by(slug: params[:id])
 
     @tag.destroy
 
     flash[:success] = "Tag successfully deleted."
 
     redirect_to tags_path
+  end
+
+private
+  def tag_params
+    params.require(:tag).permit(:name, :slug)
   end
 end
